857a7109a3359a3efd9103af9c54661a84885aa4
sutesumit <sumitsute9@gmail.com>
2025-01-21 02:41:26 +0530

    scrollbar thin

--- src/app/my_components/image_bodies/marathi_writings.tsx
+++ src/app/my_components/image_bodies/marathi_writings.tsx
@@ -6,11 +6,11 @@ import { flashFictions } from './flash_fiction_object';
 const MarathiWritings = () => {
   return (
     <div className='marathi-writings-container font-rajdhani text-[--primary-white] max-w-[100ch] mx-auto m-2 px-4 sm:px-6'>
-      <div className="all-cards flex overflow-x-auto snap-x snap-mandatory w-full scrollbar-hide gap-4 py-[1px]">
+      <div className="all-cards flex overflow-x-auto snap-x snap-mandatory w-full scrollbar-hide gap-4 py-[1px] scrollbar-thin scrollbar-thumb-[--primary-blue] scrollbar-track-transparent">
         {flashFictions.map((fiction, index) => (
           <div key={index} className="card bg-[--primary-blue] rounded-sm w-full flex-shrink-0 snap-center">
             <div className="flex flex-col-reverse md:flex-row sm:flex-row items-start gap-4 p-6">
-              <div className="card-content md:w-[50%] sm:w-[50%] aspect-square overflow-y-auto">
+              <div className="card-content md:w-[50%] sm:w-[50%] aspect-square overflow-y-auto scrollbar-thin scrollbar-thumb-[--primary-blue] scrollbar-track-transparent">
                 <h3 className='card-title text-center sticky z-10 top-0 bg-[--primary-blue] p-1'>{fiction.title}</h3>
                 <h4 className="date text-xs text-white opacity-50 text-center p-1">{fiction.date}</h4>
                 <p className="card-para text-xs leading-relaxed whitespace-pre-wrap px-4">{fiction.content}</p>
