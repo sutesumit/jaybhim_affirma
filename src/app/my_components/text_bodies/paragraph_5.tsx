474a2b5ce10816ee8752622477835964df7cebc8
sutesumit <sumitsute9@gmail.com>
2025-01-22 15:07:46 +0530

    created lunchbox page

--- src/app/my_components/text_bodies/paragraph_5.tsx
+++ src/app/my_components/text_bodies/paragraph_5.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Paragraph_5 = () => {
   return (
     <div className='paragraph-container text-container'>
-      <p>In today&apos;s times, however, <a href="\writings" className='link-text'>inspiring people</a> to take an intimate and visual look at their personal lives as a step toward healing has become almost redundant, thanks to the scale and influence of social media and its influencers.</p>
+      <p>In today&apos;s times, however, <a href="\writings" className='link-text'>inspiring people</a> to take an intimate and visual look at their <a href="/Lunchboxes" className='link-text'>personal lives</a> as a step toward healing has become almost redundant, thanks to the scale and influence of social media and its influencers.</p>
     </div>
   )
 }
