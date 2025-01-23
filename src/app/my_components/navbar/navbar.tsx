82b4a317ec6c804a736be7218b310fc5cda9f2d8
sutesumit <sumitsute9@gmail.com>
2025-01-23 23:07:30 +0530

    created empty menulinks

--- src/app/my_components/navbar/navbar.tsx
+++ src/app/my_components/navbar/navbar.tsx
@@ -1,17 +1,33 @@
+'use client';
 import React from 'react'
 import Seperator from '../Seperator'
+import MenuLinks from './menu';
 
 const Navbar = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+    
   return (
     <>
-        <div className='relative text-[var(--primary-blue)] opacity-40 hover:opacity-100 hover:bg-[var(--primary-blue)] hover:text-[var(--primary-white)] text-xs font-title p-2 w-screen text-center'>
-            <div className='nav-content flex justify-between px-4'>
-                <h2>sumit sute</h2>
-                <h2>works + projects</h2>
-                <h2>bengaluru, in</h2>
+        <a href="./">
+            <div className='relative text-[var(--primary-blue)] z-30 p-2 w-screen cursor-pointer'
+                onMouseEnter={toggleMenu}>
+                <div className='nav-content flex justify-between px-4 py-2 rounded-sm border-[1px] opacity-40 text-xs font-title hover:opacity-100 hover:border-[var(--primary-blue)]'>
+                    <h2>sumit sute</h2>
+                    <h2>works + projects</h2>
+                    <h2>bengaluru, in</h2>
+                </div>
+                <Seperator />
             </div>
-            <Seperator />
-        </div>
+        </a>
+        
+        {isMenuOpen &&
+            <MenuLinks toggleMenu={toggleMenu}/>
+        }
     </>
     
   )
