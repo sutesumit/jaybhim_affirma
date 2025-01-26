5aa2fd61c0d5da419f527764002516974de13d6c
sutesumit <sumitsute9@gmail.com>
2025-01-26 18:09:31 +0530

    menu order changed

--- src/app/my_components/navbar/linkObject.ts
+++ src/app/my_components/navbar/linkObject.ts
@@ -17,32 +17,32 @@ const linkObject = [
     },
     {
         id: 3,
-        title: 'निरंजन // Niranjan',
-        href: '/Niranjan-In-A-City',
-        image: '/thumbnails/3.jpg',
-        description: 'Niranjan’s marathi poems and my slow 15sec gif triptychs on conceit and urban loneliness through the context of career aspirations.'
-    },
-    {
-        id: 4,
         title: 'घरात घर // Homing Home',
         href: '/Homing-Home',
         image: '/thumbnails/4.jpg',
         description: ' As I imagine a visual presence of my friends on the walls of my parental house, the digital phoniness of my attempts fail to antidote my loneliness during Covid-19.'
     },
     {
+        id: 4,
+        title: 'Lunchboxes',
+        href: '/Lunchboxes',
+        image: '/thumbnails/28.jpg',
+        description: 'A collection of personal lunchboxes, each one a visual and emotional record of the author’s life.'
+    },
+    {
         id: 5,
+        title: 'निरंजन // Niranjan',
+        href: '/Niranjan-In-A-City',
+        image: '/thumbnails/3.jpg',
+        description: 'Niranjan’s marathi poems and my slow 15sec gif triptychs on conceit and urban loneliness through the context of career aspirations.'
+    },
+    {
+        id: 6,
         title: 'मराठी लघुकथा // Marathi Flash Fiction',
         href: '/Marathi-Short-Stories',
         image: '/thumbnails/12.jpg',
         description: 'Few convoluted flash-fictions titled by abstract visuals, in a secret pursuit of articulating & visualizing disturbed relationships with the self and others.'
     },
-    {
-        id: 6,
-        title: 'Lunchboxes',
-        href: '/Lunchboxes',
-        image: '/thumbnails/28.jpg',
-        description: 'A collection of personal lunchboxes, each one a visual and emotional record of the author’s life.'
-    }
 ]
 
 export default linkObject;
