a33330a9ababb5c10e9f4668f14045bfd06e533b
sutesumit <sumitsute9@gmail.com>
2025-01-26 13:42:10 +0530

    updated proof read all texts

--- src/app/my_components/text_bodies/paragraph_6.tsx
+++ src/app/my_components/text_bodies/paragraph_6.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Paragraph_6 = () => {
   return (
     <div className='paragraph-container text-container'>
-      <p>Yet, the exploration of the political within the personal remains largely obscure. Can there be interactive art forms that inspire frameworks and lenses to examine personal archives, revealing the strong undertones of our inherited emotions? Can a digital experience derived from personal digital diaries—distinct from a social media feed—compete for your attention/reflection in a meaningful way? Could websites be reimagined as a medium for artistic expression in this context?</p>
+      <p>Yet, the exploration of the political within the personal remains largely obscure. Can there be interactive art forms that inspire frameworks and lenses to examine personal archives, revealing the strong undertones of our inherited emotions? Can a digital experience derived from personal digital diaries—distinct from a social media feed—compete for our attention/reflection in a meaningful way? Could websites be reimagined as a medium for artistic expression in this context?</p>
     </div>
   )
 }
