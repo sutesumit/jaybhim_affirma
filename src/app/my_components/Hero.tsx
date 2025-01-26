b9f3c746fece1374d0bc753c9cc193d2b949a0fc
sutesumit <sumitsute9@gmail.com>
2025-01-26 14:35:46 +0530

    hero section hover mask elements updated

--- src/app/my_components/Hero.tsx
+++ src/app/my_components/Hero.tsx
@@ -14,14 +14,14 @@ const Hero = () => {
     <section className='bg-[var(--primary-white)] text-[--primary-blue] p-4
                         h-screen w-screen flex flex-col justify-center items-center
                         font-title text-xs'>
-        <div className="absolute top-[15vh] z-10 affirma-band h-[10vh] w-[70vw] rounded-xl overflow-clip flex justify-center items-center cursor-crosshair">
-          <MaskContainer className='w-full h-full' size={2} revealSize={70}>
+        <div className="absolute top-[15vh] z-10 affirma-band h-[10vh] w-[70vw] rounded-xl overflow-clip flex justify-center items-center cursor-none">
+          <MaskContainer className='w-full h-full' size={10} revealSize={200}>
             <h3 className='text-center font-body text-xs font-thin'>This website by <em>Sumit Sute</em> was created as an application for the AFFIRMA Awards 2025.</h3>
           </MaskContainer>
         </div>
-        <div className="absolute bottom-[6vh] right-[6vh] h-[10vh] w-[10vh] rounded-full cursor-crosshair">
-          <MaskContainer className='w-full h-full' size={2} revealSize={50}>
-            <Image src='/ashoka-chakra-white.svg' alt='Ashoka Chakra' width={100} height={100}/>
+        <div className="absolute bottom-[6vh] right-[6vh] h-[10vh] w-[10vh] rounded-full padding-2 overflow-hidden hover:animate-spin hover:repeat-1 cursor-none">
+          <MaskContainer className='w-full h-full' size={10} revealSize={100}>
+            <Image src='/ashoka-chakra-white.svg' className='p-[2px]' alt='Ashoka Chakra' width={100} height={100}/>
           </MaskContainer>
         </div>
       <EvervaultCard className='font-jaibhim w-[70vw]' text='जय भीम'/>
