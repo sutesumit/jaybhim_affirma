3be939d095657ee87e6152ca132975175be63394
sutesumit <sumitsute9@gmail.com>
2025-01-28 23:56:15 +0530

    not found page updated with css animation

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,20 +5,18 @@ export default function NotFound() {
     <div className="min-h-screen flex flex-col items-center justify-center px-4">
       
       <div className="404-container flex flex-col items-center justify-center mx-2 p-10 rounded-lg font-rajdhani text-center overflow-clip bg-white/50 backdrop-blur-sm border-[1px] border-[var(--primary-blue)] hover:scale-90 hover:shadow-[8px_8px_0px_0px_var(--primary-blue)] transition-transition duration-300 ease-in-out">
-          <div className='loader mb-5'></div>
+          <div className='loader2 mb-5'></div>
           <div className="space-y-6">
             <p className="text-container !text-center">
-              Arre! This page is under construction.
+            Arre! Looks like you've lost your way.
             </p>
-            <p className="text-container !text-xs !text-center !leading-relaxed"><span className='italic opacity-50'>Or just classic 404 moment?</span><br />But worry not, sumit is rolling up his sleeves,
-            chasing down time and resources, and will bring this page to life soon.<br />Hang tight!</p>
-          
+            <p className="text-container !text-xs !text-center !leading-relaxed"><span className='italic opacity-50'>Or maybe there used to be something here?</span><br />This might be because work titles and URLs were recently rearranged. Check out the Works + Projects section to find what you need!</p>
             <div className='text-container flex justify-center gap-2 font-title bg-blend-difference transition-colors duration-1000 ease-out'>
               <Link
                 href="/"
                 className="flex justify-center items-center flex-1 px-4 py-2 text-center rounded-sm border-[1px] border-[var(--primary-blue)] text-xs hover:bg-[var(--primary-blue)] hover:text-[var(--primary-white)]"
               >
-                Let&#39;s go home.
+                Let&#39;s take you back home.
               </Link>
               {/* <Link
                 href="/"
