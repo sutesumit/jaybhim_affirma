21879b4bb6766dfe996d4400c347ecd135b5b037
sutesumit <sumitsute9@gmail.com>
2025-02-03 14:22:16 +0530

    get in touch appended to the last para

--- src/app/my_components/text_bodies/paragraph_7_extended.tsx
+++ src/app/my_components/text_bodies/paragraph_7_extended.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Paragraph_7_extended = () => {
   return (
     <div className='paragraph-container text-container !mt-0 !pt-0 !pb-10'>
-      <p>I am currently seeking ways to secure the time and resources needed to further extend and refine my technical craft. It could also help me connect with fellow collaborators who, like me, are patiently waiting to engineer their ideas for the online world beyond social media.</p>
+      <p>I am currently seeking ways to secure the time and resources needed to further extend and refine my technical craft. It could also help me connect with fellow collaborators who, like me, are patiently waiting to engineer their ideas for the online world beyond social media. Let's get in touch sometime and discuss our ideas!</p>
     </div>
   )
 }
