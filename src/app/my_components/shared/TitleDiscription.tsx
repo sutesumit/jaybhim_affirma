d7f094e8d18f67ad87b9ecfbf620aaded65cf15b
sutesumit <sumitsute9@gmail.com>
2025-03-05 15:34:22 +0530

    title discription page margin top added

--- src/app/my_components/shared/TitleDiscription.tsx
+++ src/app/my_components/shared/TitleDiscription.tsx
@@ -6,7 +6,7 @@ const TitleDiscription = ({ title, description, background }: { title: string, d
     <>
         <div className='title-container z-0 relative w-full p-4 sm:p-7 m-auto overflow-hidden'>
             {background && <div className='absolute inset-0 z-[-10]'>{background}</div>}
-            <div className='max-w-[90ch] md:m-auto text-[--primary-blue] bg-white/50 backdrop-blur-sm border-[1px] border-[--primary-blue] rounded-sm'>
+            <div className='max-w-[90ch] md:mx-auto mt-7 text-[--primary-blue] bg-white/50 backdrop-blur-sm border-[1px] border-[--primary-blue] rounded-sm'>
               <div className='font-title text-center pt-6'>{title}</div>
               <div className='description m-5 flex flex-col md:flex-row max-h-[60vh] p-5 gap-12 justify-between place-items-start border-[1px] border-dotted border-[--primary-blue] rounded-sm overflow-y-auto scrollbar-thin'>
                 {description.eng && <p className='eng-description flex-1 font-rajdhani whitespace-pre-wrap z-0'>{description.eng}</p>}
