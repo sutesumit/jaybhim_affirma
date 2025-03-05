7ecb09c58776d7df7b83072b1c4ef2f4d509d516
sutesumit <sumitsute9@gmail.com>
2025-03-05 15:29:05 +0530

    header bg given

--- src/app/my_components/common/Header.tsx
+++ src/app/my_components/common/Header.tsx
@@ -17,18 +17,18 @@ const Navbar = () => {
     <nav className="navbar fixed isolate top-0 z-20 w-full">
         <Link href="/">
             <div 
-                className='relative w-full text-[var(--primary-blue)] z-30 p-2 cursor-pointer'
+                className='w-full relative text-[var(--primary-blue)] z-30 p-2 cursor-pointer'
                 onMouseEnter={toggleMenu}
             >
-                <div className='nav-content relative cursor-none flex justify-between px-4 py-2 rounded-sm border-[1px] opacity-40 text-xs font-title bg-blend-difference hover:opacity-100 hover:bg-[var(--primary-blue)] hover:text-[var(--primary-white)] transition-colors duration-1000 ease-out'>
+                <div className='nav-content cursor-none flex justify-between px-4 py-2 rounded-sm border-[1px] border-[var(--primary-blue)] text-xs font-title bg-white/50 backdrop-blur-sm hover:opacity-100 hover:bg-[var(--primary-blue)] hover:text-[var(--primary-white)] transition-colors duration-1000 ease-out'>
                     <span>sumitsute</span>
                     <span>works + projects</span>
                     <span>bengaluru, in</span>
                     <Pointer />
                 </div>
-                <div className="separator-wrapper">
+                {/* <div className="separator-wrapper">
                     <Seperator />
-                </div>
+                </div> */}
             </div>
         </Link>
         {isMenuOpen &&
--- src/app/my_components/common/navbar/menu.tsx
+++ src/app/my_components/common/navbar/menu.tsx
@@ -56,7 +56,7 @@ const MenuLinks: React.FC<MenuLinkProps> = ({ toggleMenu, setMenuOpen, isMenuOpe
                                             key={object.id}
                                             onMouseEnter={()=>setHoveredCard(object.id)}
                                             onMouseLeave={()=>setHoveredCard(null)}
-                                            className="link-card relative h-[30vh] flex-shrink-0 rounded-lg p-5 flex flex-col justify-center items-center font-rajdhani text-center overflow-clip bg-white/50 backdrop-blur-sm border-[1px] border-[var(--primary-blue)] hover:scale-90 hover:shadow-[8px_8px_0px_0px_var(--primary-blue)] transition-transition duration-300 ease-in-out"
+                                            className="link-card relative h-[30vh] flex-shrink-0 rounded-sm p-5 flex flex-col justify-center items-center font-rajdhani text-center overflow-clip bg-white/50 backdrop-blur-sm border-[1px] border-[var(--primary-blue)] hover:scale-90 hover:shadow-[8px_8px_0px_0px_var(--primary-blue)] transition-transition duration-300 ease-in-out"
                                     >
                                         <span className={hoveredCard === object.id ? 'text-[var(--primary-white)] bg-[var(--primary-blue)] px-2 py-1 rounded-sm' : ''}>{object.title}</span>
                                         {hoveredCard === object.id && <span className='text-[var(--primary-blue)] text-sm p-2'>{object.description}</span>}
