e924361ac2b96e48330d0788d16738e212c4c314
sutesumit <sumitsute9@gmail.com>
2025-03-05 22:49:12 +0530

    link addresses updated to art.sumitsute.com

--- src/app/my_components/specific/text_bodies/paragraph_4.tsx
+++ src/app/my_components/specific/text_bodies/paragraph_4.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Paragraph_4 = () => {
   return (
     <div className='paragraph-container text-container'>
-      <p>I wanted to create <LinkPreview url="https://jaybhim-affirma.vercel.app/Home-Photobook">art forms</LinkPreview> that not only resonate with people but also make them feel, &ldquo;I, too, can and should create art like this.&rdquo; As a result, creating photo objects from familial and personal images captured on mobile phone cameras became my preferred form and medium for a significant period.</p>
+      <p>I wanted to create <LinkPreview url="http://www.art.sumitsute.com/home-photobook">art forms</LinkPreview> that not only resonate with people but also make them feel, &ldquo;I, too, can and should create art like this.&rdquo; As a result, creating photo objects from familial and personal images captured on mobile phone cameras became my preferred form and medium for a significant period.</p>
     </div>
   )
 }
