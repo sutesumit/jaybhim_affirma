7e150d0bb5d0d9001dc5468c22f4724de89d0a17
sutesumit <sumitsute9@gmail.com>
2025-03-06 15:50:21 +0530

    navbar onclick redirected to homepage

--- src/app/my_components/common/Header.tsx
+++ src/app/my_components/common/Header.tsx
@@ -21,6 +21,7 @@ const Navbar = () => {
         <>
             <div 
                 className='w-full relative text-[var(--primary-blue)] z-30 p-2 pb-1 cursor-pointer'
+                onClick={() => window.location.href = '/'}
                 onMouseEnter={toggleMenu}
             >
                 <div className='nav-content cursor-pointer flex justify-between px-4 py-2 rounded-sm border-[1px] border-[var(--primary-blue)] text-xs font-title bg-white/50 backdrop-blur-sm hover:opacity-100 hover:bg-[var(--primary-blue)] hover:text-[var(--primary-white)] transition-colors duration-1000 ease-out'>
