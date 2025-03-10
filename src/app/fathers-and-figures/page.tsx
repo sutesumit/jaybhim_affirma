e3ee3e0ab6da30eca6082e2079b092257776bc1d
sutesumit <sumitsute9@gmail.com>
2025-03-10 21:23:52 +0530

    border for the reel video

--- src/app/fathers-and-figures/page.tsx
+++ src/app/fathers-and-figures/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
       </div>
 
       <div className="relative isolate min-h-screen text-container flex md:flex-row flex-col items-center justify-center gap-5 p-5">
-        <div className="flex-1 h-[80vh] w-full">
+        <div className="flex-1 h-[80vh] w-full p-1 border-[1px] border-[var(--primary-blue)] rounded-sm overflow-hidden">
           <iframe
             className='rounded-sm inline h-full overflow-hidden'
             src='https://www.youtube.com/embed/4yjNqLRRPxE?&controls=0&loop=1&playlist=4yjNqLRRPxE&modestbranding=1&showinfo=0&hl=en'
@@ -85,7 +85,7 @@ export default function Home() {
             >
             </iframe>
         </div>
-        <div className='flex-1 flex flex-col gap-2 h-[80vh] w-full'>
+        <div className='storyboard flex-1 flex flex-col gap-2 h-[80vh] w-full'>
           <textarea defaultValue={'Describe your story here!'} className='text-center text-sm flex-1 p-5 w-full bg-white/50 backdrop-blur-sm border-[1px] border-[var(--primary-blue)] rounded-sm' readOnly></textarea>
           <button onClick={()=> alert('This feature is still cooking—almost there!')} className='border-[1px] text-xs rounded-sm border-[var(--primary-blue)] p-1 hover:bg-[var(--primary-blue)] hover:text-[var(--primary-white)] transition-all duration-300'>Send me your story!</button>  
         </div>        
