dda9e23165b50304ef5333ecd74ab26a6d8b424f
sutesumit <sumitsute9@gmail.com>
2025-03-12 22:24:36 +0530

    instruction added to the playground in fathers page

--- src/app/fathers-and-figures/page.tsx
+++ src/app/fathers-and-figures/page.tsx
@@ -89,6 +89,7 @@ export default function Home() {
             </iframe>
         </div>
         <div className='storyboard flex-1 flex flex-col gap-2 h-[80vh] md:h-full w-full'>
+        <button onClick={()=> alert('This feature is still cooking—almost there!')} className='border-[1px] text-xs rounded-sm border-[var(--primary-blue)] p-1 hover:scale-90 hover:shadow-[4px_4px_0px_0px_var(--primary-blue)] transition-transition duration-300 ease-in-out'>Screeshot the story!</button>  
           <textarea defaultValue={'Describe your story here!'} className='text-center text-sm flex-1 p-5 w-full border-[1px] border-[var(--primary-blue)] rounded-sm' readOnly></textarea>
           <textarea defaultValue={'Your name (Optional)'} className='text-center text-sm p-1 w-full border-[1px] border-[var(--primary-blue)] rounded-sm' readOnly></textarea>
           <button onClick={()=> alert('This feature is still cooking—almost there!')} className='border-[1px] text-xs rounded-sm border-[var(--primary-blue)] p-1 hover:scale-90 hover:shadow-[4px_4px_0px_0px_var(--primary-blue)] transition-transition duration-300 ease-in-out'>Send me your story!</button>  
