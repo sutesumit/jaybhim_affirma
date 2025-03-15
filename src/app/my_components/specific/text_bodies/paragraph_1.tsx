db955e0c250d765fda2b6e97f293b1fd6102f754
sutesumit <sumitsute9@gmail.com>
2025-03-15 15:28:14 +0530

    link previews updated on homepage

--- src/app/my_components/specific/text_bodies/paragraph_1.tsx
+++ src/app/my_components/specific/text_bodies/paragraph_1.tsx
@@ -8,7 +8,7 @@ const Paragraph_1 = () => {
     <div className='text-container paragraph-container'>
       <div className="text-content">
         I used to be an artist, and{' '}
-        <LinkPreview url="https://jaybhim-affirma.vercel.app/Fathers-and-Figures">
+        <LinkPreview url="https://www.art.sumitsute.com/fathers-and-figures">
           my art practices
         </LinkPreview>
         {' '}were always a medium to explore the political within the personal, driven by a clear intent to actively heal from caste-patriarchal traumas. My goal was to celebrate Dalit cultural capital with pride and intimacy through my photographs. Before I fully understood the phrase &ldquo;the personal is political,&rdquo; I aspired for my art to serve as both fodder and fuel for political discourse.
