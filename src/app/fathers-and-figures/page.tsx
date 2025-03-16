aa5421c85bef7b0cef185218e3145289c5fd8b01
sutesumit <sumitsute9@gmail.com>
2025-03-16 22:16:17 +0530

    horizontal snap scrolling added to the stories on fathers page

--- src/app/fathers-and-figures/page.tsx
+++ src/app/fathers-and-figures/page.tsx
@@ -108,16 +108,16 @@ export default function Home() {
         <div className=" text-container my-1 p-1 border-[1px] border-[var(--primary-blue)] hover:bg-[var(--primary-blue)] hover:text-[var(--primary-white)] rounded-sm transition-all duration-300 ease-in-out">
             <div className='text-xs opacity-80 text-center'>In the quiet queue, <span className='story-count '>0</span> fresh stories wait their turn, joining those already shared below.</div>
         </div>
-        <div className='stories text-center flex text-container flex-row gap-2 !px-0 !py-4 !overflow-x-auto'>
-          <div className='story-container w-full flex-shrink-0 p-3 text-xs border-[1px] border-[var(--primary-blue)] rounded-sm'>
+        <div className='stories flex flex-row snap-mandatory snap-x text-center text-container gap-2 !px-0 !py-4 !overflow-x-auto'>
+          <div className='story-container snap-center w-full flex-shrink-0 p-3 text-xs border-[1px] border-[var(--primary-blue)] rounded-sm'>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda, perspiciatis? Ea facere cum tenetur perferendis iure eos, maxime esse eaque error unde, fugiat debitis natus laborum sunt perspiciatis nulla iusto!</p>
             <p className='opacity-65 italic'>- <span>Name</span></p>
           </div>
-          <div className='story-container w-full flex-shrink-0 p-3 text-xs border-[1px] border-[var(--primary-blue)] rounded-sm'>
+          <div className='story-container snap-center w-full flex-shrink-0 p-3 text-xs border-[1px] border-[var(--primary-blue)] rounded-sm'>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda, perspiciatis? Ea facere cum tenetur perferendis iure eos, maxime esse eaque error unde, fugiat debitis natus laborum sunt perspiciatis nulla iusto!</p>
             <p className='opacity-65 italic'>- <span>Name</span></p>
           </div>
-          <div className='story-container w-full flex-shrink-0 p-3 text-xs border-[1px] border-[var(--primary-blue)] rounded-sm'>
+          <div className='story-container snap-center w-full flex-shrink-0 p-3 text-xs border-[1px] border-[var(--primary-blue)] rounded-sm'>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda, perspiciatis? Ea facere cum tenetur perferendis iure eos, maxime esse eaque error unde, fugiat debitis natus laborum sunt perspiciatis nulla iusto!</p>
             <p className='opacity-65 italic'>- <span>Name</span></p>
           </div>
