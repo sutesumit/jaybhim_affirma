40cfd649768aa283c0714b51d6d2e834cf36a2df
sutesumit <sumitsute9@gmail.com>
2025-03-19 02:10:54 +0530

    handwriting font appplied to the testimonial cards in fathers page

--- src/app/fathers-and-figures/SubmitCards.tsx
+++ src/app/fathers-and-figures/SubmitCards.tsx
@@ -6,28 +6,23 @@ import submitStories from './SubmitObjects'
 
 const SubmitCards = () => {
 
-    const [ hoveredCard, setHoveredCard ] = React.useState<Number | null>(null)
-
-
   return (
     <div className='relative isolate p-5'>
         <div className="text-container my-1 p-1 border-[1px] border-[var(--primary-blue)] hover:bg-[var(--primary-blue)] hover:text-[var(--primary-white)] rounded-sm transition-all duration-300 ease-in-out">
             <div className='text-xs opacity-80 text-center'>In the quiet queue, <span className='story-count '>0</span> fresh stories wait their turn, joining those already shared below.</div>
         </div>
         <div
-            className='all-stories h-[25rem] text-container overflow-x-auto snap snap-y snap-mandatory'
+            className='all-stories h-[25rem] text-container overflow-x-auto scroll-smooth'
         >
             {[...submitStories, ...submitStories, ...submitStories, ...submitStories].map((story, index) => (
                     <div 
                         key={index} 
                         className={`story-container card-shadow sticky my-4 card-bg flex flex-col snap-center px-3 text-xs border-[1px] border-[var(--primary-blue)] rounded-sm transition-all duration-300`}
-                        onMouseEnter={() => setHoveredCard(index)}
-                        onMouseLeave={() => setHoveredCard(null)}
                         style={{ top: `${index}rem`, rotate: `${Math.random()*3 - 1.5}deg` }}
                     >
                         <div className='card flex flex-col items-end h-auto overflow-y-auto'>
                             
-                            <p className='m-auto max-w-[50ch] p-4'>
+                            <p className='m-auto text-lg font-semibold p-4 font-handwriting'>
                                 {story.story}
                                 <span  
                                     className='mx-2 opacity-50 italic before:content-["-"]'>
