53d0c550a1c988690fcadaaf6a5de67d9e8182f1
sutesumit <sumitsute9@gmail.com>
2025-03-18 14:12:55 +0530

    story testimonials made responsive on fathers page

--- src/app/fathers-and-figures/SubmitCards.tsx
+++ src/app/fathers-and-figures/SubmitCards.tsx
@@ -1,6 +1,7 @@
-import { motion as m, animate, useMotionValue } from 'framer-motion'
+import { motion as m, animate, useMotionValue, AnimatePresence } from 'framer-motion'
 import React, { useEffect } from 'react'
 import submitStories from './SubmitObjects'
+
 
 
 const SubmitCards = () => {
@@ -9,38 +10,37 @@ const SubmitCards = () => {
 
 
   return (
-    <div>
-      <div className='relative isolate p-5'>
-        <div className=" text-container my-1 p-1 border-[1px] border-[var(--primary-blue)] hover:bg-[var(--primary-blue)] hover:text-[var(--primary-white)] rounded-sm transition-all duration-300 ease-in-out">
+    <div className='relative isolate p-5'>
+        <div className="text-container my-1 p-1 border-[1px] border-[var(--primary-blue)] hover:bg-[var(--primary-blue)] hover:text-[var(--primary-white)] rounded-sm transition-all duration-300 ease-in-out">
             <div className='text-xs opacity-80 text-center'>In the quiet queue, <span className='story-count '>0</span> fresh stories wait their turn, joining those already shared below.</div>
         </div>
-        <div  className='stories text-center text-container !px-0 overflow-hidden'>
-            <m.div
-                className='flex w-full'
-            >
-                {[...submitStories, ...submitStories].map((story, index) => (
-                    
+        <div
+            className='flex md:flex-row flex-col all-stories text-container gap-2 h-[70vh] overflow-x-auto snap snap-x snap-mandatory'
+        >
+            {[...submitStories, ...submitStories, ...submitStories, ...submitStories].map((story, index) => (
+                    <div 
+                        key={index} 
+                        className={`story-container flex flex-col snap-center p-3 text-xs border-[1px] border-[var(--primary-blue)] rounded-sm`}
+                        onMouseEnter={() => setHoveredCard(index)}
+                        onMouseLeave={() => setHoveredCard(null)}
+                    >
                         <div 
-                            key={index} 
-                            className='story-container flex-grow snap-center p-3 text-xs border-[1px] border-[var(--primary-blue)] rounded-sm'
-                            onMouseEnter={() => setHoveredCard(index)}
-                            onMouseLeave={() => setHoveredCard(null)}
+                            className='m-auto h-10 w-10 flex flex-shrink-0 items-center justify-center bg-[var(--primary-blue)] text-[var(--primary-white)] rounded-full'
                         >
-                            {
-                            hoveredCard == index ?
-                            <div className='hovered-state'>
-                                <p>{story.story}</p>
-                                <p className='opacity-65 italic'>- <span>{story.name}</span></p>
-                            </div>
-                            :
-                            <div className='text-center'>card</div>
-                            }
+                            {index+1}
                         </div>
-                    )   
-                )}
-            </m.div>
+                        {
+                        hoveredCard == index &&
+                            <div className='hovered-state h-full overflow-y-auto'>
+                                <p className='opacity-65 text-center'><span  className='before:content-["-"] after:content-["-"]'>{story.name}</span></p>
+                                <p>{story.story}</p>
+                            </div>
+                        
+                        }
+                    </div>
+                )   
+            )}
         </div>
-      </div>
     </div>
   )
 }
