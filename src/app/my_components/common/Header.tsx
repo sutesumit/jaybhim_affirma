c45f47cfb3693c5e8c776407b8d13586af2c68c3
sutesumit <sumitsute9@gmail.com>
2025-03-19 21:26:43 +0530

    navbar spacing redone with grid

--- src/app/my_components/common/Header.tsx
+++ src/app/my_components/common/Header.tsx
@@ -24,15 +24,15 @@ const Navbar = () => {
                 onClick={() => window.location.href = '/'}
                 onMouseEnter={toggleMenu}
             >
-                <div className='nav-content cursor-pointer flex justify-between px-4 py-2 rounded-sm border-[1px] border-[var(--primary-blue)] text-xs font-title bg-white/50 backdrop-blur-sm hover:opacity-100 hover:bg-[var(--primary-blue)] hover:text-[var(--primary-white)] transition-colors duration-1000 ease-out'>
-                    <span>
-                    <Link href='/' className='router-tab !pr-0 !mr-0'>{'art.'}</Link><Link target='_blank' href='https://www.sumitsute.com/' className='router-tab !pl-0 !ml-0'>{'sumitsute.com'}</Link>
+                <div className='nav-content cursor-pointer grid grid-cols-3 px-4 py-2 rounded-sm border-[1px] border-[var(--primary-blue)] text-xs font-title bg-white/50 backdrop-blur-sm hover:opacity-100 hover:bg-[var(--primary-blue)] hover:text-[var(--primary-white)] transition-colors duration-1000 ease-out'>
+                    <span className='col-span-1 text-left hidden md:inline-block'>
+                        <Link href='/' className='router-tab !pr-0 !mr-0'>{'art.'}</Link><Link target='_blank' href='https://www.sumitsute.com/' className='router-tab !pl-0 !ml-0'>{'sumitsute.com'}</Link>
                     </span>
-                    <span>
+                    <span className='col-span-3 md:col-span-1 text-center'>
                         <Link href='/' className='router-tab'>{'works + projects'}</Link>
                         
                     </span>
-                    <span>
+                    <span className='col-span-1 text-right hidden md:inline-block'>
                         { <Link href={pathName} className='router-tab'>{pathName !=='/' ? tabName : "Home"}</Link> }
                     </span>
                     {/* <Pointer /> */}
