ddb20a0d3c58f76fcd4cb8204ba6aad448e5efbf
sutesumit <sumitsute9@gmail.com>
2025-03-20 16:09:23 +0530

    navbar onclick home address disabled

--- src/app/my_components/common/Header.tsx
+++ src/app/my_components/common/Header.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         <>
             <div 
                 className='w-full relative text-[var(--primary-blue)] z-30 p-2 pb-1 cursor-pointer'
-                onClick={() => window.location.href = '/'}
+                // onClick={() => window.location.href = '/'}
                 onMouseEnter={toggleMenu}
             >
                 <div className='nav-content cursor-pointer grid grid-cols-3 items-center px-4 py-2 rounded-sm border-[1px] border-[var(--primary-blue)] text-xs font-title bg-white/50 backdrop-blur-sm hover:opacity-100 hover:bg-[var(--primary-blue)] hover:text-[var(--primary-white)] transition-colors duration-1000 ease-out'>
@@ -48,6 +48,7 @@ const Navbar = () => {
                         <Link 
                             href='/' 
                             className='router-tab'
+                            onClick={() => toggleMenu()}
                         >
                                 <span className='inline-block align-middle text-sm'>
                                     <BsViewList />
