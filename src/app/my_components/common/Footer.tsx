868d059dce6550e4025b94f3a633720452951411
sutesumit <sumitsute9@gmail.com>
2025-03-29 20:33:00 +0530

    import addresses fixed for pointer element

--- src/app/my_components/common/Footer.tsx
+++ src/app/my_components/common/Footer.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react'
 import Seperator from '../shared/Seperator';
-import Pointer from './navbar/nav_pointer';
 
 interface FooterProps {}
 
