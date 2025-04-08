ddd84a542c8ea7a8bacd2688f7db98837dae7f5a
sutesumit <sumitsute9@gmail.com>
2025-04-08 18:02:49 +0530

    Code chunks in the Header element refactored

--- src/app/my_components/common/Header.tsx
+++ src/app/my_components/common/Header.tsx
@@ -1,42 +1,7 @@
 'use client';
 import React from 'react'
-import NavMenuCardsWrapper from '@/app/my_components/common/layout/NavMenu/NavMenuCards';
-import { AnimatePresence } from 'framer-motion';
-import { useNavMenu } from '@/app/my_components/common/layout/context/NavMenu/useNavMenu';
 import { NavMenuProvider } from '@/app/my_components/common/layout/context/NavMenu/NavMenuContext';
-import NavLinks from './NavLinks';
-import { usePathname } from 'next/navigation';
-
-const Navbar: React.FC = () => {
-
-    const { isMenuOpen, setMenuOpen } = useNavMenu();
-    const pathName = usePathname()
-    var tabName = pathName ? pathName.split('/').pop()?.split('-').join(' ') : "Home"
-    
-  return (
-    <nav
-        className="navbar fixed isolate top-0 z-20 w-full"
-        onMouseEnter={() => setMenuOpen(true)}
-        onMouseLeave={() => setMenuOpen(false)}
-    >
-        
-        <>
-            <div 
-                className='w-full relative text-[var(--primary-blue)] z-30 p-2 pb-1 cursor-pointer'
-            >
-                <div className='nav-content cursor-pointer grid grid-cols-3 items-center px-4 py-2 rounded-sm border-[1px] border-[var(--primary-blue)] text-xs font-title bg-white/50 backdrop-blur-sm hover:opacity-100 hover:bg-[var(--primary-blue)] hover:text-[var(--primary-white)] transition-colors duration-1000 ease-out'>
-                    <NavLinks pathName={pathName} tabName={tabName} />
-                </div>
-            </div>
-        </>
-        <AnimatePresence>
-            {isMenuOpen &&
-                <NavMenuCardsWrapper />
-            }
-        </AnimatePresence>
-    </nav>
-  )
-}
+import Navbar from './NavBar';
 
 const NavbarWrapper: React.FC = () => {
     return (
