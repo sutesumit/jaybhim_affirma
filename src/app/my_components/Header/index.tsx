8fdf6df64b1eead9e4435e8f1cb5dd8fd7a01eed
sutesumit <sumitsute9@gmail.com>
2025-04-09 18:31:55 +0530

    refactor(header): move NavBar to MenuBarWrapper

--- src/app/my_components/Header/index.tsx
+++ src/app/my_components/Header/index.tsx
@@ -1,12 +1,12 @@
 'use client';
 import React from 'react'
 import { NavMenuProvider } from '@/app/my_components/common/layout/context/NavMenu/NavMenuContext';
-import Navbar from '../common/NavBar';
+import MenuBarWrapper from './MenuBar/MenuBarWrapper';
 
 const Header: React.FC = () => {
     return (
         <NavMenuProvider>
-            <Navbar />
+            <MenuBarWrapper />
         </NavMenuProvider>
     )
 }
