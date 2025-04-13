7398e9e5952e4094b062d59d3c52120251857dae
sutesumit <sumitsute9@gmail.com>
2025-04-14 02:52:53 +0530

    refactor(fathers page): add barrel file Background component

---
+++ src/app/(pages)/fathers-and-figures/Background/index.ts
@@ -0,0 +1,5 @@
+export { default as Background } from './Background';
+
+export { generateTrail } from './generateTrail';
+export { useMouseTrail } from './useMouseTrail';
+export { default as TrailingImage } from './TrailingImage';
