d0c1091a39f39a2d6c50307b7520e8c7fd6de5c2
sutesumit <sumitsute9@gmail.com>
2025-04-24 17:49:22 +0530

    feat(card-image-preview): integrate Next.js Image component with motion for optimized rendering

--- src/app/my_components/Header/DropdownMenu/CardImagePreview.tsx
+++ src/app/my_components/Header/DropdownMenu/CardImagePreview.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
+"use client"
+
+import React, { forwardRef } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
+import Image, { ImageProps } from 'next/image'
 
 interface CardImagePreviewProps {
     object: {
@@ -12,6 +15,10 @@ interface CardImagePreviewProps {
     y: number
 }
 
+const MotionImage = motion.create(forwardRef<HTMLImageElement, ImageProps>((props, ref) => {
+    return <Image ref={ref} {...props}/>
+}))
+
 const CardImagePreview: React.FC<CardImagePreviewProps> = ({ object, hoveredCard, x, y}) => {
   return (
     <div
@@ -19,13 +26,13 @@ const CardImagePreview: React.FC<CardImagePreviewProps> = ({ object, hoveredCard
     >
         <AnimatePresence>
             {hoveredCard === object.id && (
-            <motion.img
+            <MotionImage
+                key={object.id}
                 initial={{ opacity: 0, scale: 0, rotate: 180 }}
                 animate={{ opacity: 1, scale: 1, rotate: 0 }}
                 exit={{ opacity: 0, scale: 0, rotate: -180 }}
                 transition={{
-                    duration: 0.3,
-                    ease: [0.4, 0, 0.2, 1]
+                    duration: 0.3
                 }}
                 className='menu-image'
                 src={object.image}
