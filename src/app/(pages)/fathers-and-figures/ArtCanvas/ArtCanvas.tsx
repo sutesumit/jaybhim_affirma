c70ec94a4a67f7e3021880f28ebc5db12f809e8c
sutesumit <sumitsute9@gmail.com>
2025-04-26 22:29:24 +0530

    refactor(fathers-page): extract usePhotoStyle custom hook from ArtCanvas component of Fathers and Figures page

--- src/app/(pages)/fathers-and-figures/ArtCanvas/ArtCanvas.tsx
+++ src/app/(pages)/fathers-and-figures/ArtCanvas/ArtCanvas.tsx
@@ -1,33 +1,27 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef } from 'react'
 import Seperator from '@/app/my_components/shared/Seperator'
 import Instruction from './Instruction'
 import DraggablePhoto from './DraggablePhoto'
-import { photoStyleProp } from './types'
+import usePhotoStyle from './usePhotoStyleHook'
 
 // ArtCanvas component for the Fathers and Figures page
 const ArtCanvas = () => {
     // Reference tag to the .art-canvas div container where dragable photo elements live
     const artCanvasRef = useRef<HTMLDivElement | null>(null)
 
-    // State to store the style properties for the dragable photo elements
-    const [photoStyle, setPhotoStyle] = useState<photoStyleProp[]>([])
+    // Custom hook to return state to store the style properties for the dragable photo elements
+    const photoStyle = usePhotoStyle()
 
-    // useEffect hook to set the initial style properties for the dragable photo elements upon component mount
-    useEffect(() => {
-        setPhotoStyle(Array.from({ length: 23}, (_, i)=>{
-            return {
-                rotate: Math.random() * 60 - 30,
-                scale: Math.random() * .6 + .8
-            }
-        }))
-    }, [])
-
-  return (
-    <div 
+    return (
+        <div 
         className='art-canvas relative flex flex-wrap gap-5 items-center justify-center p-[5vh] min-h-screen w-screen bg-gradient-to-tl from-blue-300 to-blue-50 rounded-lg overflow-hidden'
         ref={artCanvasRef}
       >
         <Instruction />
+        {/* Generate 23 draggable photocomponets dynamically
+            1. Create an array of 23 elements from 0 to 22
+            2. Map over the array and generate 23 draggable photocomponets dynamically
+        */}
         {Array.from({length: 23}, (_,i) => i).map((i) =>{
             return (
                 <DraggablePhoto
---
+++ src/app/(pages)/fathers-and-figures/ArtCanvas/usePhotoStyleHook.ts
@@ -0,0 +1,21 @@
+    import { useState, useEffect } from "react"
+    import { photoStyleProp } from "./types"
+
+    const usePhotoStyle = () => {
+        // State to store the style properties for the draggable photo elements
+        const [photoStyle, setPhotoStyle] = useState<photoStyleProp[]>([])
+
+        // useEffect hook to set the initial style properties for the dragable photo elements upon component mount
+        useEffect(() => {
+            setPhotoStyle(Array.from({ length: 23}, (_, i)=>{
+                return {
+                    rotate: Math.random() * 60 - 30,
+                    scale: Math.random() * .6 + .8
+                }
+            }))
+        }, [])
+
+        return photoStyle
+    }
+
+    export default usePhotoStyle
