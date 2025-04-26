2e523eb24c0c5b92ed6b0884a0d1fa300103fbd0
sutesumit <sumitsute9@gmail.com>
2025-04-26 14:33:49 +0530

    docs: add inline docs for exports in the Fathers and Figures barrel file

--- src/app/(pages)/fathers-and-figures/Background/index.ts
+++ src/app/(pages)/fathers-and-figures/Background/index.ts
@@ -1,5 +1,8 @@
-export { default as Background } from './Background';
 
+// Components for the Fathers and Figures background and mouse trail effect
+export { default as Background } from './Background';
 export { generateTrailImage } from './generateTrailImage';
+
+// Utility function and custom hook for generating and managing mouse trail images
 export { useMouseTrail } from './useMouseTrail';
 export { default as TrailingImage } from './TrailingImage';
