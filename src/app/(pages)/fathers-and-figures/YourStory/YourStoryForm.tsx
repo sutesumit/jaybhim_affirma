46bd6e2a28ddad49423d4541424fc53fa1eaa86d
sutesumit <sumitsute9@gmail.com>
2025-04-27 17:11:25 +0530

    feature(your-story): add iteractivity to form data and temporarily store form data

--- src/app/(pages)/fathers-and-figures/YourStory/YourStoryForm.tsx
+++ src/app/(pages)/fathers-and-figures/YourStory/YourStoryForm.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
 
 const YourStoryForm = () => {
+
+  const submissions: Object[] = []
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const data = Object.fromEntries(formData.entries())
+    if (data.story === '') {
+      alert('Please enter a story!')
+      return
+    }
+    submissions.push(data)
+    console.log(submissions)
+    
+  }
+  
   return (
-    <div className='storyboard flex-1 flex flex-col gap-2 h-[80vh] md:h-full w-full'>
+    <form onSubmit={handleSubmit} className='storyboard flex-1 flex flex-col gap-2 h-[80vh] md:h-full w-full'>
         <button onClick={()=> alert('sumit is still cooking this feature—almost there!')} className='border-[1px] text-xs rounded-sm border-[var(--primary-blue)] p-1 hover:scale-90 hover:shadow-[4px_4px_0px_0px_var(--primary-blue)] transition-transition duration-300 ease-in-out'>Screeshot the story!</button>  
-        <textarea defaultValue={'Describe your story here!'} className='text-center text-sm flex-1 p-5 w-full border-[1px] border-[var(--primary-blue)] rounded-sm' readOnly></textarea>
-        <textarea defaultValue={'Your name (Optional)'} className='text-center text-sm p-1 w-full border-[1px] border-[var(--primary-blue)] rounded-sm' readOnly></textarea>
-        <button onClick={()=> alert('sumit is still cooking this feature—almost there!')} className='border-[1px] text-xs rounded-sm border-[var(--primary-blue)] p-1 hover:scale-90 hover:shadow-[4px_4px_0px_0px_var(--primary-blue)] transition-transition duration-300 ease-in-out'>Send me your story!</button>  
-    </div> 
+        <textarea placeholder={'Describe your story here!'} name='story' className='text-justify placeholder:justify-center text-xs flex-1 p-5 w-full border-[1px] border-[var(--primary-blue)] rounded-sm'></textarea>
+        <textarea placeholder={'Your name (Optional)'} name='name' className='text-center text-xs p-1 border-[1px] border-[var(--primary-blue)] rounded-sm'></textarea>
+        <button type='submit' className='border-[1px] text-xs rounded-sm border-[var(--primary-blue)] p-1 hover:scale-90 hover:shadow-[4px_4px_0px_0px_var(--primary-blue)] transition-transition duration-300 ease-in-out'>Save your story!</button>
+        {/* <button onClick={()=> alert('sumit is still cooking this feature—almost there!')} className='border-[1px] text-xs rounded-sm border-[var(--primary-blue)] p-1 hover:scale-90 hover:shadow-[4px_4px_0px_0px_var(--primary-blue)] transition-transition duration-300 ease-in-out'>Save your story!</button> */}
+    </form> 
   )
 }
 
