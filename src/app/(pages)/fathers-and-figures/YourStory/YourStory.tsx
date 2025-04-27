0ef13a741da0a757cc9593e3991784728e7cd621
sutesumit <sumitsute9@gmail.com>
2025-04-27 19:38:08 +0530

    feature(your-story): expose ArtCanvas DOM via useImperrativeHandle hook, prop drilling and forwardRef to YourStoryCanvas for screenshot feature

--- src/app/(pages)/fathers-and-figures/YourStory/YourStory.tsx
+++ src/app/(pages)/fathers-and-figures/YourStory/YourStory.tsx
@@ -3,12 +3,20 @@ import React from 'react'
 import InstructionReel from './InstructionReel'
 import YourStoryForm from './YourStoryForm'
 
-const YourStory = () => {
+interface YourStoryProps {
+  artCanvasRef: React.RefObject<HTMLDivElement | null>;
+}
+
+// This component is responsible for rendering the YourStory section of the page
+// It includes the InstructionReel and YourStoryForm components, which are used to display instructions and a form for users to submit their stories
+// The artCanvasRef prop is passed down to the YourStoryForm component, allowing it to access the ArtCanvas element directly
+
+const YourStory: React.FC<YourStoryProps> = ({ artCanvasRef }) => {
   return (
     <>
         <div className="relative isolate text-container grid md:grid-cols-2 my-2 md:flex-row flex-col items-center justify-center gap-5">
             <InstructionReel />
-            <YourStoryForm />    
+            <YourStoryForm artCanvasRef={artCanvasRef} />
         </div>
 
         <div className="relative">
--- src/app/(pages)/fathers-and-figures/YourStory/YourStoryForm.tsx
+++ src/app/(pages)/fathers-and-figures/YourStory/YourStoryForm.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
 
-const YourStoryForm = () => {
+interface YourStoryFormProps {
+  artCanvasRef: React.RefObject<HTMLDivElement | null>;
+}
+
+// This component is responsible for rendering the form where users can submit their stories and names
+// It includes a textarea for the story, an optional name field, and a submit button
+const YourStoryForm: React.FC<YourStoryFormProps> = ({ artCanvasRef }) => {
+
+  // Reference tag to the .art-canvas div container where dragable photo elements live
+  // This ref is used to access the DOM element directly, allowing for manipulation or measurement of the element
+
 
   const submissions: Object[] = []
 
@@ -14,17 +24,24 @@ const YourStoryForm = () => {
     }
     submissions.push(data)
     console.log(submissions)
-    
   }
+
+
+  const handleScreenshot = () => {
+    console.log('Screenshot taken!')
+    console.log(artCanvasRef.current)
+  }
+
   
   return (
+    <>
     <form onSubmit={handleSubmit} className='storyboard flex-1 flex flex-col gap-2 h-[80vh] md:h-full w-full'>
-        <button onClick={()=> alert('sumit is still cooking this feature—almost there!')} className='border-[1px] text-xs rounded-sm border-[var(--primary-blue)] p-1 hover:scale-90 hover:shadow-[4px_4px_0px_0px_var(--primary-blue)] transition-transition duration-300 ease-in-out'>Screeshot the story!</button>  
+        <button type='button' onClick={() => handleScreenshot()} className='border-[1px] text-xs rounded-sm border-[var(--primary-blue)] p-1 hover:scale-90 hover:shadow-[4px_4px_0px_0px_var(--primary-blue)] transition duration-300 ease-in-out'>Screeshot the story!</button>  
         <textarea placeholder={'Describe your story here!'} name='story' className='text-justify placeholder:justify-center text-xs flex-1 p-5 w-full border-[1px] border-[var(--primary-blue)] rounded-sm'></textarea>
-        <textarea placeholder={'Your name (Optional)'} name='name' className='text-center text-xs p-1 border-[1px] border-[var(--primary-blue)] rounded-sm'></textarea>
+        <input type='text' placeholder={'Your name (Optional)'} name='name' className='text-center text-xs p-2 border-[1px] border-[var(--primary-blue)] rounded-sm'></input>
         <button type='submit' className='border-[1px] text-xs rounded-sm border-[var(--primary-blue)] p-1 hover:scale-90 hover:shadow-[4px_4px_0px_0px_var(--primary-blue)] transition-transition duration-300 ease-in-out'>Save your story!</button>
-        {/* <button onClick={()=> alert('sumit is still cooking this feature—almost there!')} className='border-[1px] text-xs rounded-sm border-[var(--primary-blue)] p-1 hover:scale-90 hover:shadow-[4px_4px_0px_0px_var(--primary-blue)] transition-transition duration-300 ease-in-out'>Save your story!</button> */}
-    </form> 
+    </form>
+    </>
   )
 }
 
