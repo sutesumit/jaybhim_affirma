1a1350ecc73aecc46602cc3b46193e9f02a93860
sutesumit <sumitsute9@gmail.com>
2025-04-28 11:46:18 +0530

    feature(your-story): add consistent random tilt to local story cards

--- src/app/(pages)/fathers-and-figures/StoryCards/StoryCard.tsx
+++ src/app/(pages)/fathers-and-figures/StoryCards/StoryCard.tsx
@@ -5,16 +5,17 @@ import { StoryObjectType } from '../types'
 // Define the StoryCard component that takes a story object as a prop and an index
 interface StoryCardProps {
   story: StoryObjectType; // The story object to be displayed in the card
+  rotation: number; // The rotation angle for the card
 }
 
 
 // Define the StoryCard component that takes a story object as a prop
 // The component is responsible for rendering a single story card with the story text and the name of the person who submitted it
-const StoryCard = ({ story }: StoryCardProps) => {
+const StoryCard = ({ story, rotation }: StoryCardProps) => {
   return (
     <div 
         className={`story-container h-full sticky card-bg flex flex-col snap-center px-3 text-xs border-[1px] border-[var(--primary-blue)] rounded-sm transition-all duration-300`}
-        style={{ top: 0 }}
+        style={{ top: 0, rotate: `${rotation}deg` }}
     >
         <div className='card flex flex-col h-full items-center justify-center overflow-y-auto'>
             <p className='m-auto text-lg font-semibold p-4 font-handwriting'>
--- src/app/(pages)/fathers-and-figures/StoryCards/StoryCardSet.tsx
+++ src/app/(pages)/fathers-and-figures/StoryCards/StoryCardSet.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import StoryCard from './StoryCard'
 import { StoryObjectType } from '../types'
 
 
 const StoryCardSet = ({ storyObject }: { storyObject: StoryObjectType[] }) => {
+
+  const [rotations, setRotations] = React.useState<number[]>([])
+
+  useEffect(() => {
+    const newRotations = storyObject.map(() => Math.floor(Math.random() * 5 - 2.5)) // Generate a random rotation between -2.5 and 2.5 degrees
+    setRotations(newRotations) 
+  }, [storyObject])
 
   return (
     <div
@@ -14,6 +21,7 @@ const StoryCardSet = ({ storyObject }: { storyObject: StoryObjectType[] }) => {
             <StoryCard 
                 key={index} 
                 story={story} 
+                rotation={rotations[index]}
             />
             )
         })}        
