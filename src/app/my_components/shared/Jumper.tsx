11c7f6357abb689da313565afe11862289b7ad36
sutesumit <sumitsute9@gmail.com>
2025-06-05 18:47:07 +0530

    style(jumper-button): refactor and enhance jumpter component with explicit texts and engaging animations

--- src/app/my_components/shared/Jumper.tsx
+++ src/app/my_components/shared/Jumper.tsx
@@ -1,27 +1,74 @@
 'use client' 
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { RiScrollToBottomFill } from 'react-icons/ri'
 import Seperator from './Seperator'
+import { Ghost, Milestone } from 'lucide-react'
+import { motion as m, AnimatePresence } from 'framer-motion'
+
+const jumperTexts = [
+  {
+    id: 'tldr',
+    label: 'TL;DR',
+    icon: <Milestone className='h-4 w-4 ml-2 animate-pierce'/>
+  },
+  {
+    id: 'artBoard',
+    label: 'Skip to Art',
+    icon: null
+  }
+]
+
 
 const Jumper = () => {
 
     const seperatorRef = useRef<HTMLDivElement>(null)
+    const [index, setIndex] = useState(0)
+
+    useEffect(()=>{
+      const interval = setInterval(()=>{
+        setIndex((prev)=> (prev + 1)% jumperTexts.length)
+      }, 5000)
+      return () => clearInterval(interval)
+    }, [])
+
+    const handleScroll = () => {
+      seperatorRef.current?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'center'
+      })
+    }
+
+    const currentJumperText = jumperTexts[index]
 
   return (
-    <div className='flex flex-col items-center cursor-pointer relative'
-
-                  onClick={() => {
-
-                    if (seperatorRef.current) {
-                        const target = seperatorRef.current
-                        target.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'center' })
-                    }
-                  }}
+    <div className='m-auto pb-5 items-center cursor-pointer relative'
+      onClick={handleScroll}
+    >
+        <div
+          className='flex relative gap-2 font-rajdhani items-center justify-center m-auto h-8 p-4 checkbox w-fit overflow-clip button-style'
+        >
+          <RiScrollToBottomFill className='text-xl'/>
+          <AnimatePresence mode='wait'>
+            <m.div
+              key={index}
+              className='justify-center'
+              initial= {{ y: 20, opacity: 0 }}
+              animate = {{ y: 0, opacity: 1 }}
+              exit={{ y: -20, opacity: 0 }}
+              transition={{ duration: 0.3, ease: 'easeOut' }}
             >
-              <RiScrollToBottomFill className='text-xl inline animate-bounce my-2 hover:text-slate-500'/>
-              <div className="w-full">
-                  <Seperator ref={seperatorRef} />
-              </div>
+              <span className='flex items-center justify-center w-20'>
+                  {currentJumperText.label}
+                  {currentJumperText.icon}
+              </span>
+            </m.div>
+          </AnimatePresence>
+        </div>
+
+      <div className="w-full">
+          <Seperator ref={seperatorRef} />
+      </div>
     </div>
   )
 }
--- src/app/my_components/shared/TitleDiscription.tsx
+++ src/app/my_components/shared/TitleDiscription.tsx
@@ -8,7 +8,7 @@ const TitleDiscription = ({ title, description, background }: { title: string, d
             {background && <div className='absolute inset-0 z-[-10]'>{background}</div>}
             <div className='max-w-[90ch] mx-2 md:mx-auto text-[--primary-blue] bg-white/50 transition-[background] duration-300 hover:bg-gradient-to-bl hover:from-blue-300 hover:via-blue-50 hover:to-blue-50 backdrop-blur-sm border-[1px] border-[--primary-blue] rounded-sm'>
             <div className='font-title text-center pt-6'>{title}</div>
-              <div className='description m-5 flex flex-col md:flex-row max-h-[60vh] hover:shadow-[inset_0px_0px_15px_-5px_rgba(59,_130,_246,_0.5)] p-5 gap-12 justify-between place-items-start border-[1px] border-dotted border-[--primary-blue] rounded-sm overflow-y-auto scrollbar-thin transition-all duration-300'>
+              <div className='description m-5 flex flex-col md:flex-row max-h-[50vh] hover:shadow-[inset_0px_0px_15px_-5px_rgba(59,_130,_246,_0.5)] p-5 gap-12 justify-between place-items-start border-[1px] border-dotted border-[--primary-blue] rounded-sm overflow-y-auto scrollbar-thin transition-all duration-300'>
                 {description.eng && <p className='eng-description flex-1 font-rajdhani whitespace-pre-wrap z-0'>{description.eng}</p>}
                 {description.mar && <p className='mar-description flex-1 font-rajdhani whitespace-pre-wrap z-0'>{description.mar}</p>}
               </div>
