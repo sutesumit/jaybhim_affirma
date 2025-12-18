1437751881a88065d83d5dadcdae2b56d723a5e8
sutesumit <sumitsute9@gmail.com>
2025-12-18 23:42:14 +0530

    style/canvasInstrution: add new menuconfigs and update thumbnails

--- src/app/my_components/Header/DropdownMenu/menuConfig.ts
+++ src/app/my_components/Header/DropdownMenu/menuConfig.ts
@@ -47,8 +47,29 @@ const menuConfig = [
         id: 7,
         title: 'कलाकाराचे निवेदन // Artist’s Statement',
         href: '/artist-statement',
-        image: '/thumbnails/12.jpg',
+        image: '/thumbnails/5.jpg',
         description: 'A collection of Artists Statements'
+    },
+    {
+        id: 8,
+        title: 'घरात घर // At Home, Elsewhere',
+        href: '/at-home-elsewhere',
+        image: '/thumbnails/4.jpg',
+        description: ' As I imagine a visual presence of my friends on the walls of my parental house, the digital phoniness of my attempts fail to antidote my loneliness during Covid-19.'
+    },
+    // {
+    //     id: 9,
+    //     title: 'A Day in the Life of Ramesh Mawaskar',
+    //     href: '/a-day-in-the-life-of-ramesh-mawaskar',
+    //     image: '/thumbnails/6.jpg',
+    //     description: 'A Day in the Life of Ramesh Mawaskar: How, my long-time friend from Melghat, spent his first day as a cold-drink merchant in a weekly market.'
+    // },
+    {
+        id: 10,
+        title: 'Notes on Atrocity Cases',
+        href: '/notes-on-atrocity-cases',
+        image: '/thumbnails/7.jpg',
+        description: 'A textual-visual field diary about documenting the stories of dalit resistance in Indian state Gujarat for thewire.in.'
     }
 ]
 
