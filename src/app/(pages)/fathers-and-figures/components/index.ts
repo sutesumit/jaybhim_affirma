6949234477132944c414b2179760fd5ce19335ee
sutesumit <sumitsute9@gmail.com>
2026-03-05 10:00:45 +0530

    feat: add missing barrel files for components
    - Create components/index.ts for fathers-and-figures
    - Create components/index.ts for home-photobook

---
+++ src/app/(pages)/fathers-and-figures/components/index.ts
@@ -0,0 +1,4 @@
+export { HeroSection, Background, generateTrailImage, useMouseTrail, TrailingImage } from './HeroSection'
+export { Submissions } from './Submissions'
+export { MaraaSection } from './MaraaSection'
+export { MyCardProvider } from './MakeMyCard'
